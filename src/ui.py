cf4a9376eb83abcb68e09de5d3de9ffcd693d446
skeltal2 <joel.weinitschke@gmail.com>
2022-02-20 17:36:15 +0200

    Päivitetty UI

--- src/ui.py
+++ src/ui.py
@@ -10,6 +10,7 @@ class UI():
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("2048")
+        self.root.resizable(0, 0)
 
         self.frm = ttk.Frame(
             master=self.root,
@@ -27,6 +28,18 @@ class UI():
 
         self.tile_font = Font(
             family="Clear Sans",
+            size=40,
+            weight="bold"
+        )
+
+        self.score_font = Font(
+            family="Clear Sans",
+            size=15,
+            weight="bold"
+        )
+
+        self.arrow_font = Font(
+            family="Source Code Pro Black",
             size=40,
             weight="bold"
         )
@@ -62,6 +75,8 @@ class UI():
         }
 
         self.do_solve = False
+        self.game_over = False
+        self.arrows = {0:"←", 1:"→", 2:"↓", 3:"↑"}
 
         self.board = Board()
         self.setup_controls()
@@ -71,32 +86,86 @@ class UI():
             master=self.control_frm,
             text="Ratkaise!",
             width=20,
-            height=4
+            height=3
+        )
+
+        new_game_button = tk.Button(
+            master=self.control_frm,
+            text="Uusi Peli",
+            width=20,
+            height=3
         )
 
         exit_button = tk.Button(
             master=self.control_frm,
             text="Lopeta",
             width=20,
-            height=4
+            height=3
         )
 
+        self.move_display = tk.StringVar()
+        self.move_display.set("-")
+
+        move_label = tk.Label(
+            master=self.control_frm,
+            textvariable=self.move_display,
+            font=self.arrow_font,
+            width=6,
+            #height=2
+        )
+
+        self.score_display = tk.StringVar()
+        self.score_display.set("Pisteet:\n0"+"Siirto:\n0")
+
+        score_label = tk.Label(
+            master=self.control_frm,
+            textvariable=self.score_display,
+            font=self.score_font,
+            width=6,
+            height=4,
+        )
+
+        self.turn_timer = tk.Scale(
+            master=self.control_frm,
+            orient="horizontal",
+            from_=1,
+            to=1000,
+        )
+        self.turn_timer.set(25)
+
         start_button.bind('<ButtonRelease>', self.solve_button)
+        new_game_button.bind('<ButtonRelease>', self.new_game_button)
         exit_button.bind('<ButtonRelease>', self.quit_button)
 
-        start_button.grid(row=0, column=0)
-        exit_button.grid(row=1, column=0)
-    
+        move_label.grid(row=2, pady=2)
+        score_label.grid(row=3, pady=2)
+        start_button.grid(row=0, pady=2)
+        new_game_button.grid(row=4, pady=2)
+        exit_button.grid(row=5, pady=2)
+        self.turn_timer.grid(row=1, pady=2)
+
     def solve_button(self, event):
         self.do_solve = not self.do_solve
+        if self.game_over:
+            self.new_game_button(None)
+            self.do_solve = True
         self.solve()
-    
+
     def quit_button(self, event):
         self.root.quit()
 
+    def new_game_button(self, event):
+        if not self.do_solve or self.game_over:
+            self.board.__init__()
+            self.game_over = False
+            self.do_solve = False
+            self.board.new_tile()
+            self.board.new_tile()
+            self.update_grid()
+
     def build_grid(self):
-        for y in range(4):
-            for x in range(4):
+        for board_y in range(4):
+            for board_x in range(4):
                 text = tk.StringVar()
                 text.set(0)
                 label = tk.Label(
@@ -108,17 +177,23 @@ class UI():
                     anchor="center",
                 )
                 label.grid(
-                    column=x,
-                    row=y,
+                    column=board_x,
+                    row=board_y,
                     padx=2,
                     pady=2
                 )
                 self.labels.append(label)
                 self.tiles.append(text)
         self.update_grid()
-    
+
     def update_grid(self):
         #väri testi [0,0,0,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192]
+        self.score_display.set(
+            "Pisteet:\n"+
+            str(self.board.get_score())+
+            "\nSiirto:\n"+
+            str(self.board.get_moves())
+        )
         new_tiles = self.board.get_list()
         for i in range(16):
             tile = new_tiles[i]
@@ -135,44 +210,52 @@ class UI():
                     b_color = "#3c3a33"
             self.labels[i].config(foreground = f_color)
             self.labels[i].config(background = b_color)
-    
+
     def keypress(self, event):
-        print(event)
         if event.keysym in self.move_map and not self.do_solve:
             self.handle_game(event.keysym)
-    
-    def handle_game(self, move):
-        if self.move_map[move] in self.board.get_legal_moves():
-            self.board.move(self.move_map[move])
+
+    def handle_game(self, move_c):
+        if self.game_over:
+            return
+        move = self.move_map[move_c]
+
+        if self.board.move(move):
+            self.move_display.set(self.arrows[move])
             self.board.new_tile()
             self.update_grid()
 
-        if max(self.board.get_list()) == 2048:
+        if max(self.board.get_list()) == 2048 and not self.game_over:
+            self.game_over = True
             showinfo(title="2048", message="Voitit Pelin!")
-            self.root.quit()
+            return
 
         if self.board.get_legal_moves() is None:
+            self.game_over = True
             showinfo(title="2048", message="Hävisit Pelin!")
-            self.root.quit()
-    
-    def solve(self, turn_time=10):
+            return
+
+    def solve(self):
         result = Minimax(self.board, True).start()
         move = result[0]
-        value = result[1]
+        #value = result[1]
 
-        if max(self.board.get_list()) == 2048:
+        if max(self.board.get_list()) == 2048 and not self.game_over:
+            self.game_over = True
             showinfo(title="2048", message="Voitit Pelin!")
-            self.root.quit()
+            return
 
         if move is None:
+            self.game_over = True
             showinfo(title="2048", message="Hävisit Pelin!")
-            self.root.quit()
-        else:
-            self.board.move(move)
-            self.board.new_tile()
-            self.update_grid()
-            if self.do_solve:
-                self.root.after(turn_time, self.solve)
+            return
+        
+        self.board.move(move)
+        self.move_display.set(self.arrows[move])
+        self.board.new_tile()
+        self.update_grid()
+        if self.do_solve:
+            self.root.after(self.turn_timer.get(), self.solve)
 
     def display(self):
         self.board.new_tile()
